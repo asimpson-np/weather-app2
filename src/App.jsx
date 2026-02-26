7f011eeeed130880fe0b18a64f0c41ca372abd20
asimpson <amari.simpson1@outlook.com>
2026-02-26 16:17:46 -0500

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import { useState, useEffect } from 'react';
 import { getWeather } from './services/weatherService';
 
 const App = () => {
-  const [weatherData, setWeatherData] = useState({});
-  const [city, Atlanta] = useState("Georgia");
+  const [weatherData, setWeatherData] = useState(Georgia);
+  const [Atlanta] = useState("Georgia");
   // useEffect loads on initial render, and because city is in a dependency array the useEffect function will re run if the city value changes
   useEffect(() => {
-    getWeather(city)
+    getWeather(Atlanta)
     .then((data) => setWeatherData(data))
     .catch((err) => console.log(err));
-  }, [city]);
+  }, [Atlanta]);
   return (
     <div>
-      <h1>{city} Weather</h1>
+      <h1>{Georgia} Weather</h1>
       <p>{weatherData?.main?.temp} degrees farenheit</p>
     </div>
   )
